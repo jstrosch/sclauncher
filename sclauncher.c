b3001225ae20c988f7efeead42430ddb2846aca3
@jstrosch <josh@m9cyber.com>
2024-02-01 13:49:59 -0600

    updates argument handling to accept hex or decimal value for entry point offset

--- sclauncher.c
+++ sclauncher.c
@@ -10,7 +10,7 @@ char shellcode[] = "";
 
 int main(int argc, char **argv) {
 	unsigned int shellcode_size = 0;
-	unsigned int offset = 0;
+	unsigned int entry_point = 0;
 	char hexcc[1] = {0x90};
 	bool insert_bp = false;
 	bool produce_pe = false;
@@ -37,8 +37,12 @@ int main(int argc, char **argv) {
 			printf("[*] Producing a PE file...\n");
 		} else if (!strncmp(argv[arg_count],"-ep",3)) {
 			command_arg = validate_argument(argv[arg_count]);
-			offset = atoi(command_arg);
-			printf("[*] Adjusting shellcode entry point: +0x%08x\n", offset);
+			if (!strncmp(command_arg, "0x", 2)){
+				entry_point = strtol(command_arg, NULL, 16);
+			} else {
+				entry_point = atoi(command_arg);
+			}
+			printf("[*] Adjusting shellcode entry point: +0x%08x\n", entry_point);
 		} else if(!strncmp(argv[arg_count],"-f",2)){
 			command_arg = validate_argument(argv[arg_count]);
 			strncpy(file_path,command_arg,strlen(command_arg));
@@ -60,7 +64,7 @@ int main(int argc, char **argv) {
 		if (fp != NULL){
 			fseek(fp, 0L, SEEK_END);
 			shellcode_size = ftell(fp);
-			if (insert_bp && offset >= shellcode_size) {
+			if (insert_bp && entry_point >= shellcode_size) {
 				printf("[!] Breakpoint entry point beyond size of shellcode, exiting!");
 				exit(1);
 			}
@@ -77,17 +81,17 @@ int main(int argc, char **argv) {
 				puts("[PE] Producing PE file from shellcode found in a file, then exiting.");
 				sc_stage = (char*)malloc(shellcode_size);
 				fread((char*)sc_stage, sizeof(char), shellcode_size, fp);
-				create_pe(sc_stage,shellcode_size, offset, is_64);
+				create_pe(sc_stage,shellcode_size, entry_point, is_64);
 				free(sc_stage);
 			} else {
 				stage = VirtualAlloc(0, shellcode_size + 1, 0x1000,0x40 );
 				printf("[*] Allocated memory at %p\n", stage);
-				if (insert_bp && offset) {
-					bytes_read = fread((char*)stage, sizeof(char), offset-1, fp);
+				if (insert_bp && entry_point) {
+					bytes_read = fread((char*)stage, sizeof(char), entry_point-1, fp);
 					printf("[*] %zu bytes of shellcode read\n", bytes_read);
-					memmove((char*)stage+offset-1, &hexcc, 1);
-					printf("[*] Breakpoint inserted at %p\n",(char*)stage+offset-1);
-					bytes_read = fread((char*)stage+offset, sizeof(char), (shellcode_size - offset +1), fp);
+					memmove((char*)stage+entry_point-1, &hexcc, 1);
+					printf("[*] Breakpoint inserted at %p\n",(char*)stage+entry_point-1);
+					bytes_read = fread((char*)stage+entry_point, sizeof(char), (shellcode_size - entry_point +1), fp);
 					printf("[*] %zu remaining bytes of shellcode read\n", bytes_read);
 				} else if (insert_bp) {
 					memmove(stage, &hexcc, 1);
@@ -109,15 +113,15 @@ int main(int argc, char **argv) {
 
 		if (produce_pe ) {
 			puts("[PE] Producing PE file from shellcode found internally, then exiting.");
-			create_pe(shellcode, shellcode_size, offset, is_64);
+			create_pe(shellcode, shellcode_size, entry_point, is_64);
 		} else {
 			stage = VirtualAlloc(0, shellcode_size + 1, 0x1000,0x40 );
 			printf("[*] Allocated memory at %p\n", stage);
 		
-			if(insert_bp && offset) {
-				memmove(stage, &shellcode, offset -1 );
-				memmove((char*) stage+offset-1, &hexcc, 1);
-				memmove((char*) stage+offset, &shellcode[offset-1],shellcode_size - offset +1);
+			if(insert_bp && entry_point) {
+				memmove(stage, &shellcode, entry_point -1 );
+				memmove((char*) stage+entry_point-1, &hexcc, 1);
+				memmove((char*) stage+entry_point, &shellcode[entry_point-1],shellcode_size - entry_point +1);
 			} else if (insert_bp) {
 				memmove(stage, &hexcc, 1);
 				memmove((char*)stage+1, &shellcode, shellcode_size);
@@ -131,9 +135,9 @@ int main(int argc, char **argv) {
 	}
 
 	if( !produce_pe) {
-		if (offset) {
-			target_addy = (char*)stage + offset - 1; //adjust for zero-based address
-			printf("[*] Adjusting offset, new entry point: 0x%p\n", target_addy);
+		if (entry_point) {
+			target_addy = (char*)stage + entry_point - 1; //adjust for zero-based address
+			printf("[*] Adjusting entry_point, new entry point: 0x%p\n", target_addy);
 		} else {
 			target_addy = stage;
 		}
